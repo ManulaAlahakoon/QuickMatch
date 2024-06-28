d307dccda7ed779772f936b35d6caad58933167a
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-06-28 21:27:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import Navbar from './components/NavigationBar';
-import './App.css';
+//import Navbar from "./components/NavigationBar";  // Homepage Navbar with sign in option
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Profile from "./components/Profile";
+import Feedback from "./components/Feedback";
+import NavigationForServiceProvider from "./components/NavigationForServiceProvider"; // Navbar of service provider panel with options unique to it
+import Notification from './components/Notification'; //Notification
+import FinancePage from './components/FinancePage';
+import NavService from './components/MainNavigationBarForServiceProvider'; // Navbar of service provider panel with log out option
+
+import "./App.css";
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      
+      <NavService/>
+
+      <Router>
+        <NavigationForServiceProvider />
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/requests" element={<Notification />} />
+          <Route path="/finance" element={<FinancePage/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+/*      
+
+<Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/services" element={<ServiceListings />} />
+      </Routes>
+    </Router>*/
+/*
+
+*/
